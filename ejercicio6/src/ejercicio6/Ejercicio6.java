e449b5127de4b5ed0fcaf15554c375c5a3aed50b
Mauricio Pulgarin <mauriciopul@gmail.com>
2019-06-13 13:08:11 -0500

    Ejercicio 6 resuelto

---
+++ ejercicio6/src/ejercicio6/Ejercicio6.java
@@ -0,0 +1,20 @@
+/*--->  ENUNCIADO OK
+hacer un algoritmo que imprima los primeros 20 términos de la siguiente serie:
+1, 3, 6, 10, 15, 21, 28,……..
+ */
+package ejercicio6;
+
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+
+        int c = 0;
+
+        for (int i = 1; i <= 20; i++) {
+            c = c + i;
+            System.out.println(i + ": " + c);
+        }
+
+    }
+
+}
